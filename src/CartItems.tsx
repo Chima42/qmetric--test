1164233c99e0a8021d241b5842e522e7b409bf72
Chima Nwosu <me@chimannwosu.com>
2021-10-11 11:10:27 +0100

    Cart refactor and fixes

--- src/CartItems.tsx
+++ src/CartItems.tsx
@@ -1,33 +1,19 @@
 import _ from "lodash";
 import { Image, List, Button } from "semantic-ui-react";
-import { ItemProps } from "./types/Item";
-
-type CartItemProps = {
-  item: ItemProps;
-  onRemove: (id: number) => number;
-};
-
-
-const getKGDescription = (amountOfItem: number, value: number, itemInfo: string) => {
-  return `£${((value * amountOfItem) / 100).toFixed(2)} (${amountOfItem}${itemInfo})`;
-}
-
-const CartItem = ({ item, onRemove }: CartItemProps) => {
-  const {name, imageSrc, description, id} = item;
-  
-};
+import { AvailableItemProps } from "./types/Item";
 
 type CartItemsProps = {
-  items: ItemProps[];
+  items: AvailableItemProps[];
+  formatPriceAndDescription: (item: AvailableItemProps) => string;
   onRemove: (id: number) => void;
 };
 
-const CartItems = ({ items, onRemove }: CartItemsProps) => (
+const CartItems = ({ items, onRemove, formatPriceAndDescription }: CartItemsProps) => (
   <List divided relaxed>
     {
       items.map((item) => {
-        const {name, imageSrc, description, id, amount, weightType} = item;
-        return _.range(amount || 0).map(() => (
+        const { name, imageSrc, id, totalAddedToCart } = item;
+        return _.range(totalAddedToCart || 0).map((index) => {
           // <CartItem
           //   key={id}
           //   name={name}
@@ -36,28 +22,19 @@ const CartItems = ({ items, onRemove }: CartItemsProps) => (
           //   onRemove={onRemove(id)}
           // />
 
-          <List.Item>
+          return <List.Item key={`${item.id}-${index}`}>
             <Image src={imageSrc} size="mini" style={{ height: 50 }} />
             <List.Content>
               <List.Header>{name}</List.Header>
-              <List.Description>{description}</List.Description>
+              <List.Description>{formatPriceAndDescription(item)}</List.Description>
             </List.Content>
             <List.Content floated="right" verticalAlign="bottom">
               <Button title={`Remove ${name}`} icon="trash" onClick={() => onRemove(id)} />
             </List.Content>
           </List.Item>
-        ))
+      })
       })
     }
-
-    {/* // {!!oranges && (
-    //   <CartItem
-    //     name="Oranges"
-    //     imageSrc="/items/oranges.svg"
-    //     description={`£${((199 * oranges) / 100).toFixed(2)} (${oranges}kg @ £1.99/kg)`}
-    //     onRemove={() => onSetItem("oranges", oranges - 1)}
-    //   />
-    // )} */}
   </List>
 );
 
--- src/CartTotal.tsx
+++ src/CartTotal.tsx
@@ -1,51 +1,61 @@
 import _ from "lodash";
 import { List, Header } from "semantic-ui-react";
+import { AvailableItemProps } from "./types/Item";
 
-type DiscountItemProps = {
+type DiscountAvailableItemProps = {
   description: string;
   saving: number;
 };
 
-const DiscountItem = ({ description, saving }: DiscountItemProps) => (
+const DiscountItem = ({ description, saving }: DiscountAvailableItemProps) => (
   <List.Item>
     <List.Content>
       <List.Header>{description}</List.Header>
-      <List.Description>{`-£${(saving / 100).toFixed(2)}`}</List.Description>
+      <List.Description>{`-£${saving.toFixed(2)}`}</List.Description>
     </List.Content>
   </List.Item>
 );
 
 type CartTotalProps = {
-  cart: { [itemId: string]: number };
+  items: AvailableItemProps[]
 };
 
-const CartTotal = ({ cart: { beans, eggs, oranges } }: CartTotalProps) => {
-  const subtotal = beans * 50 + eggs * 295 + oranges * 199;
-  const savings = Math.round(beans / 3) * 50;
+const CartTotal = ({ items }: CartTotalProps) => {
+  const subtotal = items.map(item => item.price * item.totalAddedToCart).reduce((previousPrice, currentPrice) => previousPrice + currentPrice);
+  const discountedItems = items.filter(item => item.discounted);
+  discountedItems.forEach(item => {
+    item.savings = Math.round(item.totalAddedToCart / 3) * item.price
+  });
+  const totalSavings = discountedItems.reduce((a,b) => a + b.savings, 0);
 
+  
   return (
     <>
-      <Header as="h3">{`Sub-total: £${(subtotal / 100).toFixed(2)}`}</Header>
-
-      {savings && (
+      <Header as="h3">{`Sub-total: £${subtotal.toFixed(2)}`}</Header>
+      {totalSavings && (
         <>
           <Header as="h2">Awesome Discounts</Header>
 
-          <List divided relaxed>
-            {_.range(Math.floor(beans / 3)).map((i) => (
-              <DiscountItem
-                key={i}
-                description="Beans: Three tins for the price of two"
-                saving={50}
-              />
-            ))}
-          </List>
+          {
+            discountedItems.map(item => {
+              return <List divided relaxed>
+                {_.range(Math.floor(item.totalAddedToCart / 3)).map((i) => (
+                  <DiscountItem
+                    key={i}
+                    description={`${item.name}: ${item.discount}`}
+                    saving={item.savings ? item.savings : 0}
+                  />
+                ))}
+              </List>
+            })
+          }
 
-          <Header as="h3">{`Total Savings: -£${(savings / 100).toFixed(2)}`}</Header>
+
+          <Header as="h3">{`Total Savings: -£${totalSavings.toFixed(2)}`}</Header>
         </>
       )}
 
-      <Header as="h2">{`Total to Pay: £${((subtotal - savings) / 100).toFixed(2)}`}</Header>
+      <Header as="h2">{`Total to Pay: £${(subtotal - totalSavings).toFixed(2)}`}</Header>
     </>
   );
 };
